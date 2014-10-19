5da48b4de6b6507be8dda46e67caa1d13743e5b3
PMinin <pavel.minin.pm@gmail.com>
2014-10-19 23:59:23 +0400

    Gauss solve method

--- com.pm.rap.toe/src/com/pm/rap/toe/BasicEntryPoint.java
+++ com.pm.rap.toe/src/com/pm/rap/toe/BasicEntryPoint.java
@@ -2,20 +2,35 @@ package com.pm.rap.toe;
 
 import org.eclipse.rap.rwt.application.AbstractEntryPoint;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 
+import com.pm.rap.toe.gauss.GaussSolver;
 
 public class BasicEntryPoint extends AbstractEntryPoint {
 
-    @Override
-    protected void createContents(Composite parent) {
-        parent.setLayout(new GridLayout(2, false));
-        Button checkbox = new Button(parent, SWT.CHECK);
-        checkbox.setText("Hello");
-        Button button = new Button(parent, SWT.PUSH);
-        button.setText("World");
-    }
+	@Override
+	protected void createContents(Composite parent) {
+		parent.setLayout(new GridLayout(2, false));
+		Button button = new Button(parent, SWT.PUSH);
+		button.setText("Solve example");
+		button.addSelectionListener(new SelectionAdapter() {
+			private static final long serialVersionUID = -5716606111780566444L;
 
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				double[] solve = GaussSolver.solve(new double[][] {
+						new double[] { 2, 1, -1, 8 },
+						new double[] { -3, -1, 2, -11 },
+						new double[] { -2, 1, 2, -3 } });
+				for (int i = 0; i < solve.length; i++) {
+					System.out.println(solve[i]);
+				}
+			}
+
+		});
+	}
 }
---
+++ com.pm.rap.toe/src/com/pm/rap/toe/gauss/GaussSolver.java
@@ -0,0 +1,24 @@
+package com.pm.rap.toe.gauss;
+
+import java.util.Arrays;
+
+public class GaussSolver {
+
+	public static double[] solve(double[][] a) {
+		double[][] x = new double[a.length][a[0].length];
+		for (int i = 0; i < a.length; i++)
+			x[i] = Arrays.copyOf(a[i], a[i].length);
+		double p;
+		for (int i = 0; i < x.length; i++)
+			for (int j = 0; j < x.length; j++)
+				if (i != j) {
+					p = x[j][i] / x[i][i];
+					for (int k = i; k <= x.length; ++k)
+						x[j][k] -= p * x[i][k];
+				}
+		double[] b = new double[x.length];
+		for (int i = 0; i < x.length; i++)
+			b[i] = x[i][x.length] / x[i][i];
+		return b;
+	}
+}
